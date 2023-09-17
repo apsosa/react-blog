dfdeacd3a021b985527ccbdc615a8d2754596f75
Patricio Sosa <patdeveloper.data@gmail.com>
2023-09-17 20:24:36 -0300

    class 21

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,12 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
-
+import useFetch from "./useFetch";
 const Home = () => {
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog)=>blog.id !== id);
-    setBlogs(newBlogs);
-  
-  }
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      }
-      )
-      .then(data => {
-        console.log(data);
-        setBlogs(data);
-      }
-      );
-  }, []);
-  const [blogs, setBlogs] = useState(null);
+  const {data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
   return (
     <div className="home">
-      {/*<BlogList blogs={blogs} title="All Blogs !!" handleDelete={handleDelete}/>*/}
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs !!"/>}
     </div>
   );
 }
