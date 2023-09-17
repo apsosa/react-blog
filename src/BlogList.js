0f066fcaaaae61911d591950deb2d034b4321e4d
Pato <patdeveloper.data@gmail.com>
2023-09-17 16:15:16 -0300

    wip

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,16 +1,16 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({blogs, title, handleDelete}) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
-      {/* {blogs.map((blog) => (
+      {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>delete Blog</button>
         </div>
-      ))} */}
-      {blogs.map((blog) => (
-        <BlogListPreview blog={blog} key={blog.id} handleDelete={handleDelete} />
       ))}
     </div>
   );
 }
+
+export default BlogList;
